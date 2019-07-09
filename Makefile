b585e99f586f1c6bef0c5eea365a5bfe5d80078b
Mylnev <Dmitry.Mylnev@Markets.com>
2019-07-09 23:15:17 +0300

    copy sql

--- Makefile
+++ Makefile
@@ -9,6 +9,7 @@ build: clean
 	go build -o ./bin/execution/execution ./cmd/execution/execution.go
 
 	cp -n ./etc/execution.properties ./bin/execution/
+	cp -n ./etc/sql/schema.sql ./bin/execution/
 
 test:
 	go test -race ./...
--- cmd/execution/execution.go
+++ cmd/execution/execution.go
@@ -18,6 +18,14 @@ func init() {
 
 func main() {
 
-	log.Info("execution is going to start")
+	log.Info("Execution is going to start")
+
+	pwd, err := os.Getwd()
+
+	if err != nil {
+		log.Fatal("Getwd error", err)
+	}
+
+	log.Debug("Current folder is [" + pwd + "]")
 
 }
