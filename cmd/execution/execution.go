ba5cb9a101914ae67e99be588fa2671b6f26359d
Mylnev <Dmitry.Mylnev@Markets.com>
2019-07-09 22:05:43 +0300

    added logger

--- cmd/execution/execution.go
+++ cmd/execution/execution.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+func init() {
+
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: true,
+		FullTimestamp: true,
+	})
+
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.TraceLevel)
+}
+
 func main() {
+
+	log.Info("execution is going to start")
 
 }
