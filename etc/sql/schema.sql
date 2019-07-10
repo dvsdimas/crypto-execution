a7206351b302f8a48d9c8dc99f0ac52c2da9e97e
Mylnev <Dmitry.Mylnev@Markets.com>
2019-07-10 22:03:49 +0300

    finished schema

--- etc/sql/schema.sql
+++ etc/sql/schema.sql
@@ -1,7 +1,8 @@
 BEGIN;
 
+
 CREATE TABLE "exchange" (
-    id SMALLSERIAL PRIMARY KEY,
+    id   SMALLSERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
 
     CONSTRAINT "exchange_to_name_unique" UNIQUE (name)
@@ -12,7 +13,7 @@ INSERT INTO "exchange" ("name") VALUES ('KRAKEN');
 
 
 CREATE TABLE "direction" (
-    id SMALLSERIAL PRIMARY KEY,
+    id    SMALLSERIAL PRIMARY KEY,
     value VARCHAR(5) NOT NULL,
 
     CONSTRAINT "direction_to_value_unique" UNIQUE (value)
@@ -23,7 +24,7 @@ INSERT INTO "direction" ("value") VALUES ('SELL');
 
 
 CREATE TABLE "order_type" (
-    id SMALLSERIAL PRIMARY KEY,
+    id   SMALLSERIAL PRIMARY KEY,
     type VARCHAR(20) NOT NULL,
 
     CONSTRAINT "order_type_to_type_unique" UNIQUE (type)
@@ -34,7 +35,7 @@ INSERT INTO "order_type" ("type") VALUES ('LIMIT');
 
 
 CREATE TABLE "time_in_force" (
-    id SMALLSERIAL PRIMARY KEY,
+    id   SMALLSERIAL PRIMARY KEY,
     type VARCHAR(5) NOT NULL,
 
     CONSTRAINT "time_in_force_to_type_unique" UNIQUE (type)
@@ -44,7 +45,7 @@ INSERT INTO "time_in_force" ("type") VALUES ('FOK');
 
 
 CREATE TABLE "execution_type" (
-    id SMALLSERIAL PRIMARY KEY,
+    id   SMALLSERIAL PRIMARY KEY,
     type VARCHAR(10) NOT NULL,
 
     CONSTRAINT "execution_type_to_type_unique" UNIQUE (type)
@@ -55,7 +56,7 @@ INSERT INTO "execution_type" ("type") VALUES ('CLOSE');
 
 
 CREATE TABLE "execution_status" (
-    id SMALLSERIAL PRIMARY KEY,
+    id    SMALLSERIAL PRIMARY KEY,
     value VARCHAR(20) NOT NULL,
 
     CONSTRAINT "execution_status_to_value_unique" UNIQUE (value)
@@ -67,6 +68,48 @@ INSERT INTO "execution_status" ("value") VALUES ('COMPLETED');
 INSERT INTO "execution_status" ("value") VALUES ('TIMED_OUT');
 INSERT INTO "execution_status" ("value") VALUES ('ERROR');
 INSERT INTO "execution_status" ("value") VALUES ('REJECTED');
+
+
+CREATE TABLE "execution" (
+    id                BIGSERIAL PRIMARY KEY,
+    exchange_id       SMALLINT NOT NULL,
+    instrument_name   VARCHAR(20) NOT NULL,
+    direction_id      SMALLINT NOT NULL,
+    order_type_id     SMALLINT NOT NULL,
+    limit_price       NUMERIC(24,10) DEFAULT NULL,
+    amount            NUMERIC(24, 10) NOT NULL,
+    status_id         SMALLINT NOT NULL,
+    connector_id      SMALLINT NOT NULL,
+    execution_type_id SMALLINT NOT NULL,
+    execute_till_time TIMESTAMP NOT NULL,
+    ref_position_id   TEXT DEFAULT NULL,
+    time_in_force_id  SMALLINT NOT NULL DEFAULT 1,
+    update_timestamp  TIMESTAMP NOT NULL,
+    account_id        BIGINT NOT NULL,
+    description       TEXT DEFAULT NULL,
+
+    CONSTRAINT "execution_fk1" FOREIGN KEY ("exchange_id")       REFERENCES "exchange"         ("id"),
+    CONSTRAINT "execution_fk2" FOREIGN KEY ("status_id")         REFERENCES "execution_status" ("id"),
+    CONSTRAINT "execution_fk3" FOREIGN KEY ("direction_id")      REFERENCES "direction"        ("id"),
+    CONSTRAINT "execution_fk4" FOREIGN KEY ("order_type_id")     REFERENCES "order_type"       ("id"),
+    CONSTRAINT "execution_fk5" FOREIGN KEY ("time_in_force_id")  REFERENCES "time_in_force"    ("id"),
+    CONSTRAINT "execution_fk6" FOREIGN KEY ("execution_type_id") REFERENCES "execution_type"   ("id")
+);
+
+
+CREATE TABLE "execution_history" (
+    id             BIGSERIAL PRIMARY KEY,
+    execution_id   BIGINT NOT NULL,
+    status_from_id SMALLINT NOT NULL,
+    status_to_id   SMALLINT NOT NULL,
+    timestamp      TIMESTAMP NOT NULL,
+    description    TEXT DEFAULT NULL,
+
+    CONSTRAINT "execution_history_fk1" FOREIGN KEY ("execution_id")   REFERENCES "execution" ("id"),
+    CONSTRAINT "execution_history_fk2" FOREIGN KEY ("status_from_id") REFERENCES "execution_status" ("id"),
+    CONSTRAINT "execution_history_fk3" FOREIGN KEY ("status_to_id")   REFERENCES "execution_status" ("id")
+);
+
 
 COMMIT;
 
