f60eada603bfc92d52080dcbd594ef9943a92dc9
Mylnev <Dmitry.Mylnev@Markets.com>
2019-07-11 21:11:04 +0300

    sql fixes

--- etc/sql/schema.sql
+++ etc/sql/schema.sql
@@ -2,72 +2,72 @@ BEGIN;
 
 
 CREATE TABLE "exchange" (
-    id   SMALLSERIAL PRIMARY KEY,
+    id   SMALLINT PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
 
     CONSTRAINT "exchange_to_name_unique" UNIQUE (name)
 );
 
-INSERT INTO "exchange" ("name") VALUES ('BINANCE');
-INSERT INTO "exchange" ("name") VALUES ('KRAKEN');
+INSERT INTO "exchange" ("id", "name") VALUES (1, 'BINANCE');
+INSERT INTO "exchange" ("id", "name") VALUES (2, 'KRAKEN');
 
 
 CREATE TABLE "direction" (
-    id    SMALLSERIAL PRIMARY KEY,
+    id    SMALLINT PRIMARY KEY,
     value VARCHAR(5) NOT NULL,
 
     CONSTRAINT "direction_to_value_unique" UNIQUE (value)
 );
 
-INSERT INTO "direction" ("value") VALUES ('BUY');
-INSERT INTO "direction" ("value") VALUES ('SELL');
+INSERT INTO "direction" ("id", "value") VALUES (1, 'BUY');
+INSERT INTO "direction" ("id", "value") VALUES (2, 'SELL');
 
 
 CREATE TABLE "order_type" (
-    id   SMALLSERIAL PRIMARY KEY,
+    id   SMALLINT PRIMARY KEY,
     type VARCHAR(20) NOT NULL,
 
     CONSTRAINT "order_type_to_type_unique" UNIQUE (type)
 );
 
-INSERT INTO "order_type" ("type") VALUES ('MARKET');
-INSERT INTO "order_type" ("type") VALUES ('LIMIT');
+INSERT INTO "order_type" ("id", "type") VALUES (1, 'MARKET');
+INSERT INTO "order_type" ("id", "type") VALUES (2, 'LIMIT');
 
 
 CREATE TABLE "time_in_force" (
-    id   SMALLSERIAL PRIMARY KEY,
+    id   SMALLINT PRIMARY KEY,
     type VARCHAR(5) NOT NULL,
 
     CONSTRAINT "time_in_force_to_type_unique" UNIQUE (type)
 );
 
-INSERT INTO "time_in_force" ("type") VALUES ('FOK');
+INSERT INTO "time_in_force" ("id", "type") VALUES (1, 'FOK');
 
 
 CREATE TABLE "execution_type" (
-    id   SMALLSERIAL PRIMARY KEY,
+    id   SMALLINT PRIMARY KEY,
     type VARCHAR(10) NOT NULL,
 
     CONSTRAINT "execution_type_to_type_unique" UNIQUE (type)
 );
 
-INSERT INTO "execution_type" ("type") VALUES ('OPEN');
-INSERT INTO "execution_type" ("type") VALUES ('CLOSE');
+INSERT INTO "execution_type" ("id", "type") VALUES (1, 'OPEN');
+INSERT INTO "execution_type" ("id", "type") VALUES (2, 'CLOSE');
 
 
 CREATE TABLE "execution_status" (
-    id    SMALLSERIAL PRIMARY KEY,
+    id    SMALLINT PRIMARY KEY,
     value VARCHAR(20) NOT NULL,
 
     CONSTRAINT "execution_status_to_value_unique" UNIQUE (value)
 );
 
-INSERT INTO "execution_status" ("value") VALUES ('CREATED');
-INSERT INTO "execution_status" ("value") VALUES ('EXECUTING');
-INSERT INTO "execution_status" ("value") VALUES ('COMPLETED');
-INSERT INTO "execution_status" ("value") VALUES ('TIMED_OUT');
-INSERT INTO "execution_status" ("value") VALUES ('ERROR');
-INSERT INTO "execution_status" ("value") VALUES ('REJECTED');
+INSERT INTO "execution_status" ("id", "value") VALUES (1, 'CREATED');
+INSERT INTO "execution_status" ("id", "value") VALUES (2, 'EXECUTING');
+INSERT INTO "execution_status" ("id", "value") VALUES (3, 'COMPLETED');
+INSERT INTO "execution_status" ("id", "value") VALUES (4, 'TIMED_OUT');
+INSERT INTO "execution_status" ("id", "value") VALUES (5, 'ERROR');
+INSERT INTO "execution_status" ("id", "value") VALUES (6, 'REJECTED');
 
 
 CREATE TABLE "execution" (
