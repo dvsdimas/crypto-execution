9b16d9b9a96e5d25fd90fffb69a939cd589ab08d
Mylnev <Dmitry.Mylnev@Markets.com>
2019-07-11 22:03:10 +0300

    added helper

--- src/msq.ai/db/postgres/helper/helper.go
+++ src/msq.ai/db/postgres/helper/helper.go
@@ -7,6 +7,40 @@ import (
 	"msq.ai/constants"
 )
 
+func CloseDb(db *sql.DB) {
+
+	if db == nil {
+		return
+	}
+
+	err := db.Close()
+
+	if err != nil {
+		log.Error("CloseDb error ", err)
+	}
+}
+
+func GetDbByUrl(url string) (*sql.DB, error) {
+
+	log.Trace("Try connect to DB ...")
+
+	db, err := sql.Open(constants.DbName, url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	log.Trace("Successfully connected to DB")
+
+	return db, nil
+}
+
 func CheckDbUrl(url string) error {
 
 	log.Trace("Try to check DB connection ...")
